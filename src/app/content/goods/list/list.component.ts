f0848b2a741dd855b373840e25332c4cdeeccbe1
alvin <kui@ankr.com>
2021-01-06 00:11:57 +0800

    goods list

--- src/app/content/goods/list/list.component.ts
+++ src/app/content/goods/list/list.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.sass']
 })
 export class ListComponent implements OnInit {
+  images = ['assets/goods/list/1.webp', 'assets/goods/list/2.webp', 'assets/goods/list/3.webp', 'assets/goods/list/4.webp', 'assets/goods/list/5.webp', 'assets/goods/list/6.webp', 'assets/goods/list/7.webp'];
 
   constructor() { }
 
